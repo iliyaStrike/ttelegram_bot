25cf274a86c52f6e94aa81a880757ed12fb38540
iliyaStrike <iliyaabedi13890304@gmail.com>
2026-07-05 14:33:19 +0330

    Add files via upload

--- database.py
+++ database.py
@@ -15,14 +15,21 @@ conn.commit()
 
 
 def add_file(code, file_id, name):
-    cursor.execute(
-        "INSERT OR REPLACE INTO files VALUES (?, ?, ?)",
-        (code, file_id, name)
-    )
+    cursor.execute("INSERT OR REPLACE INTO files VALUES (?, ?, ?)",
+                   (code, file_id, name))
     conn.commit()
 
 
 def get_file(code):
     cursor.execute("SELECT file_id FROM files WHERE code=?", (code,))
-    result = cursor.fetchone()
-    return result[0] if result else None
+    return cursor.fetchone()
+
+
+def get_all():
+    cursor.execute("SELECT code, name FROM files")
+    return cursor.fetchall()
+
+
+def delete_file(code):
+    cursor.execute("DELETE FROM files WHERE code=?", (code,))
+    conn.commit()
