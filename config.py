dfc2b288bec87db378a3280353a7d50cdd26d63c
iliyaStrike <iliyaabedi13890304@gmail.com>
2026-07-05 14:21:12 +0330

    Add files via upload

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_1 = os.getenv("CHANNEL_1")
 CHANNEL_2 = os.getenv("CHANNEL_2")
-FILE_ID = os.getenv("FILE_ID")
+
+ADMIN_ID = os.getenv("ADMIN_ID")
