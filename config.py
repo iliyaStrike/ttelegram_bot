dd4dc8694bcaa560793e4e7889e70bc9a0edb4f7
iliyaStrike <iliyaabedi13890304@gmail.com>
2026-07-05 16:03:47 +0330

    Add files via upload

--- config.py
+++ config.py
@@ -2,11 +2,10 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-CHANNEL_1 = int(os.getenv("CHANNEL_1"))   # آیدی عددی
-CHANNEL_2 = os.getenv("CHANNEL_2")        # @username
+CHANNEL_1 = int(os.getenv("CHANNEL_1"))      # آیدی عددی
+CHANNEL_1_LINK = os.getenv("CHANNEL_1_LINK") # لینک دعوت
 
-CHANNEL_1_LINK = os.getenv("CHANNEL_1_LINK")
-CHANNEL_2_LINK = os.getenv("CHANNEL_2_LINK")
-
+CHANNEL_2 = os.getenv("CHANNEL_2")           # @username
+CHANNEL_2_LINK = os.getenv("CHANNEL_2_LINK") # لینک عمومی
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 BOT_USERNAME = os.getenv("BOT_USERNAME")
