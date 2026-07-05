ab4550451965cb8bb649e14fa8d6e2840b260348
iliyaStrike <iliyaabedi13890304@gmail.com>
2026-07-05 16:00:00 +0330

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_1 = os.getenv("CHANNEL_1")
-CHANNEL_2 = os.getenv("CHANNEL_2")
 
-ADMIN_ID = os.getenv("ADMIN_ID")
+CHANNEL_1 = int(os.getenv("CHANNEL_1"))   # آیدی عددی
+CHANNEL_2 = os.getenv("CHANNEL_2")        # @username
+
+CHANNEL_1_LINK = os.getenv("CHANNEL_1_LINK")
+CHANNEL_2_LINK = os.getenv("CHANNEL_2_LINK")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 BOT_USERNAME = os.getenv("BOT_USERNAME")
